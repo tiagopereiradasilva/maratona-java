5ef71b216a33456c8ed5132e07de006a0b558d52
Tiago Pereira <tiagopereiracx@gmail.com>
2022-05-07 14:50:33 -0300

    Aula 03: Exercicio sobre tipos primitivos

---
+++ src/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,14 @@
+package maratonajava.introducao;
+
+import java.util.Date;
+
+public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String nome = "Maria da Silva";
+        String endereco = "Rua do Mangueirão, 604, Vila Alecrim";
+        float salario = 10000f;
+        String dataRecebimento = "07/05/2022";
+        System.out.println("Eu "+nome+", morando no endereço "+endereco+", confirmo que recebi o salário de "+salario+" na data "+dataRecebimento);
+
+    }
+}
