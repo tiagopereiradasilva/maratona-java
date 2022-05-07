10d754e5460d4a1f3efc6d57e8dcc531ec3553b8
Tiago Pereira <tiagopereiracx@gmail.com>
2022-05-07 14:22:14 -0300

    Aula 02: Tipos primitivos

---
+++ src/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,16 @@
+package maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        int idade = 27;
+        System.out.println("A idade é: "+idade+" anos");
+        double salarioDouble = 10000;
+        float salarioFloat = 2500;
+        char caracter = 'M';
+        byte idadeByte = -1;
+        short idadeShort = 10;
+        long numeroMuitoGrande = 100000;
+        boolean valorLogico = true;
+    }
+}
