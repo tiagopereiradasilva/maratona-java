667c610c48e6af055a4c98bd952ee9b239bf6cbc
Tiago Pereira <tiagopereiracx@gmail.com>
2022-05-08 13:37:19 -0300

    Aula 07: Foreach em array

---
+++ src/maratonajava/introducao/Aula07Arrays03.java
@@ -0,0 +1,10 @@
+package maratonajava.introducao;
+
+public class Aula07Arrays03 {
+    public static void main(String[] args) {
+        int [] numeros = {1,2,3,4,5};
+        for(int numero : numeros){
+            System.out.println(numero);
+        }
+    }
+}
