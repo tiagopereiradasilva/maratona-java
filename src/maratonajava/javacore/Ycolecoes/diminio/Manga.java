81cd3b37cf3cb28a7c3d9673e336f8bffd49ff53
Tiago Pereira <tiagopereiracx@gmail.com>
2022-05-15 10:54:00 -0300

    inserção do atributo quantidade

--- src/maratonajava/javacore/Ycolecoes/diminio/Manga.java
+++ src/maratonajava/javacore/Ycolecoes/diminio/Manga.java
@@ -6,11 +6,17 @@ public class Manga implements Comparable<Manga>{
     private Long id;
     private String nome;
     private Double preco;
+    private int quantidade;
 
     public Manga(Long id, String nome, Double preco) {
         this.id = id;
         this.nome = nome;
         this.preco = preco;
+    }
+
+    public Manga(Long id, String nome, Double preco, int quantidade) {
+        this(id, nome, preco);
+        this.quantidade = quantidade;
     }
 
     @Override
@@ -19,6 +25,7 @@ public class Manga implements Comparable<Manga>{
                 "id=" + id +
                 ", nome='" + nome + '\'' +
                 ", preco=" + preco +
+                ", quantidade=" + quantidade +
                 '}';
     }
 
@@ -27,12 +34,19 @@ public class Manga implements Comparable<Manga>{
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Manga manga = (Manga) o;
-        return Objects.equals(id, manga.id) && Objects.equals(nome, manga.nome) && Objects.equals(preco, manga.preco);
+        return Objects.equals(id, manga.id) && Objects.equals(nome, manga.nome) && Objects.equals(preco, manga.preco) && Objects.equals(quantidade, manga.quantidade);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, nome, preco);
+        return Objects.hash(id, nome, preco, quantidade);
+    }
+
+    @Override
+    public int compareTo(Manga outroManga) {
+        //return this.id.compareTo(outroManga.getId());
+        return this.nome.compareTo(outroManga.getNome());
+        //return this.preco.compareTo(outroManga.getPreco());
     }
 
     public Long getId() {
@@ -59,11 +73,14 @@ public class Manga implements Comparable<Manga>{
         this.preco = preco;
     }
 
-    @Override
-    public int compareTo(Manga outroManga) {
-        //return this.id.compareTo(outroManga.getId());
-        return this.nome.compareTo(outroManga.getNome());
-        //return this.preco.compareTo(outroManga.getPreco());
+    public int getQuantidade() {
+        return quantidade;
     }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+
 
 }
